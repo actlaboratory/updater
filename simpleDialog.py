9a8ee22a33b41663104820f2ab0bcb61f1ff5f9c
guredora <contact@guredora.com>
2020-05-31 19:25:47 +0900

    requirements.txtの追加。

--- simpleDialog.py
+++ simpleDialog.py
@@ -3,27 +3,5 @@
 #Copyright (C) 2020 guredora <contact@guredora.com>
 
 import ctypes
-import winsound
-import wx
-import constants
-
-def winDialog(title,message):
+def dialog(title,message):
 	ctypes.windll.user32.MessageBoxW(0,message,title,0x00000040)
-def qDialog(message, title="確認"):
-	dialog = wx.MessageDialog(None,message,title,wx.YES_NO|wx.ICON_QUESTION)
-	winsound.MessageBeep(winsound.MB_ICONQUESTION)
-	result = dialog.ShowModal()
-	dialog.Destroy()
-	return result
-def dialog(message, title=constants.APP_NAME):
-	dialog = wx.MessageDialog(None,message,title,wx.OK|wx.ICON_INFORMATION)
-	winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-	dialog.ShowModal()
-	dialog.Destroy()
-	return
-def errorDialog(message):
-	dialog = wx.MessageDialog(None, message, _("エラー"), wx.OK|wx.ICON_ERROR)
-	winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
-	dialog.ShowModal()
-	dialog.Destroy()
-	return
