444917a6ae4f83a6adc8e177ec5e564757d04587
guredora <contact@guredora.com>
2020-06-07 12:39:33 +0900

    通信処理を呼び出し元アプリケーションへ完全移行

--- updater.py
+++ updater.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2020 guredora <contact@guredora.com>
 import os
 import argparse
-import urllib.request
 import subprocess
 import constants
 from simpleDialog import *
@@ -19,7 +18,7 @@ def exchandler(type, exc, tb):
 sys.excepthook=exchandler
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("arg1")
+parser.add_argument("arg1")# アプリケーションのフルパスを指定
 parser.add_argument("arg2")#アップデート確認の時はソフトのバージョンをアップデートするときはダウンロードurlを指定。
 parser.add_argument("wakeWord")#constants.wakeWordに設定してある文字列を指定。これが設定した物と違うと動かない。
 args = parser.parse_args()
@@ -30,15 +29,13 @@ if args.wakeWord == constants.wakeWord:
 	if os.path.basename(args.arg1) in process_list:# アプリケーションの修了確認
 		dialog("アップデートを行う前に%sを終了してください" % (os.path.basename(args.arg1)), "エラー")
 		sys.exit()
-	up_name = os.path.basename(args.arg2)	#URLからファイル名を取得
-	urllib.request.urlretrieve(args.arg2, up_name)#アップデータをダウンロード
 	proc = subprocess.run(					#アップデータ実行
-		(up_name, "-y"),
+		(args.arg2, "-y"),
 		stdout=subprocess.PIPE,
 		stderr=subprocess.STDOUT,
 		text=True
 	)
-	os.remove(up_name)					#アップデータを削除
+	os.remove(args.arg2)					#アップデータを削除
 	dialog("アップデートが正常に完了しました。アプリケーションを起動します。", "完了")
 	subprocess.Popen((args.arg1))# アプリケーションを起動
 
