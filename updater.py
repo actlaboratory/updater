41e1def3d2f5d2fde92b1ae1f1f7bbfb6739483f
guredora <contact@guredora.com>
2020-10-21 20:45:18 +0900

    アップデーターのハッシュ判定に対応

--- updater.py
+++ updater.py
@@ -9,6 +9,7 @@ import psutil
 from time import sleep
 import sys
 import zipfile
+import hashlib
 
 def exchandler(type, exc, tb):
 	dialog("エラーが発生しました", "エラー")
@@ -19,10 +20,11 @@ def exchandler(type, exc, tb):
 	f.close()
 sys.excepthook=exchandler
 
-parser = argparse.ArgumentParser(add_help=False)
+parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument("arg1")# アプリケーションのフルパスを指定
 parser.add_argument("wakeWord")#constants.wakeWordに設定してある文字列を指定。これが設定した物と違うと動かない。
 parser.add_argument("arg2")# アップデート用のzipファイルの場所を指定
+parser.add_argument("hash")# 改ざん防止にアップデーターのsha1ハッシュを指定
 
 args = parser.parse_args()
 if args.wakeWord == constants.wakeWord:
@@ -32,6 +34,16 @@ if args.wakeWord == constants.wakeWord:
 	if os.path.basename(args.arg1) in process_list:# アプリケーションの修了確認
 		dialog("アップデートを行う前に%sを終了してください" % (os.path.basename(args.arg1)), "エラー")
 		sys.exit()
+	if not os.path.exists(args.arg2):
+		dialog("アップデーターが見つかりません。", "エラー")
+		sys.exit()
+	with open(args.arg2, mode="rb") as f:
+		up_hash = hashlib.sha1(f.read()).hexdigest()
+		print(up_hash)
+		print(args.hash)
+		if up_hash != args.hash:
+			dialog("アップデーターのハッシュが登録されたものと一致しません。糸せず表示された場合は作者までご連絡ください。", "エラー")
+			sys.exit()
 	with zipfile.ZipFile(args.arg2) as up:
 		up.extractall(os.getcwd())
 	os.remove(args.arg2)					#アップデータを削除
