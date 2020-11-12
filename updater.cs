b3d60e2a4048cab04627a1e68d841a91de53ac37
guredora <contact@guredora.com>
2020-11-12 19:33:02 +0900

    updaterを改良

--- updater.cs
+++ updater.cs
@@ -1,11 +1,12 @@
-*/
+/*
 actlab updater
 Copyright (C) 2020 guredora <contact@guredora.com>
-/*
+
+*/
 
 using System;
 using System.IO;
-using System.IO.Compression;
+using Ionic.Zip;
 using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.Diagnostics;
@@ -13,7 +14,7 @@ using System.Diagnostics;
 class program{
 	//updater起動時に指定するwakeWord
 	public const string SETTING_WAKEWORD = "hello";
-	public static int Main(string[] args){
+	public static int run(string[] args){
 		if(args.Length < 5){
 			return 0;//コマンドライン引数が足りない
 		}
@@ -26,8 +27,13 @@ class program{
 			MessageBox.Show("wakeWordが正しくありません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			return 1;//終了
 		}
-		using (Process application = Process.GetProcessById(pid)){//起動元プロセスの取得
-			application.WaitForExit();//終了まで待機
+		try{
+			using (Process application = Process.GetProcessById(pid)){
+				application.WaitForExit();//呼び出し元アプリケーションの終了待機
+			}
+		}
+		catch(ArgumentException e){
+			Console.WriteLine(e.Message);
 		}
 		if(!File.Exists(updater_file)){//updaterが存在しない
 			MessageBox.Show("指定されたアップデーターが見つかりません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -40,11 +46,27 @@ class program{
 				MessageBox.Show("アップデーターのハッシュが指定されたものと一致しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return 3;//終了
 			}
-			//zipを展開する処理
+			file.Seek(0, SeekOrigin.Begin);//streamの開始位置をリセット
+			using(ZipFile zip = ZipFile.Read(file, System.Text.Encoding.GetEncoding("shift_jis"))){//zipFileを開く
+				zip.ExtractAll(".", ExtractExistingFileAction.OverwriteSilently);//展開
+			}
 		}
 		MessageBox.Show("アップデートが完了しました。アプリケーションを起動します。", "updater", MessageBoxButtons.OK, MessageBoxIcon.Information);
 		System.Diagnostics.Process.Start(application_name);//アプリケーション起動
 		File.Delete(updater_file);//updaterの削除
 		return 0;//正常終了
+	}
+
+	public static int Main(string[] args){
+		try{
+			return run(args);
+		}
+		catch(Exception e){
+			MessageBox.Show("エラーが発生しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			using (StreamWriter writer = new StreamWriter("updater_errorLog.txt", false, System.Text.Encoding.GetEncoding("shift_jis"))){
+				writer.WriteLine(e.ToString());
+			}
+			return 1;
+		}
 	}
 }
